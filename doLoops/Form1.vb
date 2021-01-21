7cb91c5ff72836d4b492402214a43335b202b5ae
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-21 14:18:17 -0600

    Initial commit

---
+++ doLoops/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intNumber As Integer = 0
+        Do
+            intNumber = intNumber + 1
+            MessageBox.Show(intNumber)
+        Loop Until intNumber = 10
+
+        intNumber = 0
+        Do
+            intNumber = intNumber + 1
+            MessageBox.Show(intNumber)
+        Loop While intNumber < 10
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim intNumber As Integer = 0
+        Do
+            intNumber = intNumber + 3
+            MessageBox.Show(intNumber)
+        Loop Until intNumber = 15
+
+        intNumber = 0
+        Do
+            intNumber = intNumber + 3
+            MessageBox.Show(intNumber)
+        Loop While intNumber < 15
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim intNumber As Integer = 5
+        Do
+            MessageBox.Show(intNumber)
+            intNumber = intNumber + 3
+        Loop Until intNumber > 27
+
+        intNumber = 5
+        Do
+            MessageBox.Show(intNumber)
+            intNumber = intNumber + 3
+        Loop While intNumber < 27
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim intNumber As Integer = 0
+        Do
+            intNumber = intNumber - 1
+            MessageBox.Show(intNumber)
+        Loop While intnumber < 0
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim intStart As Integer
+        Dim intStop As Integer
+        Dim intSkip As Integer
+        Dim intNumber As Integer
+        intStart = InputBox("Enter A Start")
+        intStop = InputBox("Enter A Stop")
+        intSkip = InputBox("Enter A Skip")
+        intNumber = intStart
+    End Sub
+End Class
---
+++ doLoops/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("doLoops")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("doLoops")> 
+<Assembly: AssemblyCopyright("Copyright ©  2020")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f9155692-b7ef-4a77-b682-18bbca84e042")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
